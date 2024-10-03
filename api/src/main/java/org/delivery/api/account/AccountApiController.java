d9b11ea0384adc34111c6ca443a04d7916307d46
delee <maryy1016@gmail.com>
2024-10-03 14:09:14 +0900

    배달 어플 초기 셋팅 config

---
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -0,0 +1,33 @@
+package org.delivery.api.account;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.db.account.AccountEntity;
+import org.delivery.db.account.AccountRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
+public class AccountApiController {
+
+    private final AccountRepository accountRepository;
+    @GetMapping("")
+    public void save(){
+        var account = AccountEntity.builder().build();
+        accountRepository.save(account);
+    }
+
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("s@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
+    }
+}
